20892c0e2f881f39ed543af54167677fb39b0977
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-13 19:55:37 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "username" {
+  description = "The username for authentication"
+  type        = string
+  default     = "hassan"
+}
+
+variable "password" {
+  description = "The password for authentication"
+  type        = string
+ default    = "Hassan@123"
+}
