4245941f7f8e23018802b8a9e1e3d39991f3d6a0
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-14 14:47:04 +0530

    Update main.tf

--- infra/modules/lambda/main.tf
+++ infra/modules/lambda/main.tf
@@ -64,7 +64,7 @@ resource "aws_api_gateway_integration" "lambda_integration" {
 
 resource "aws_api_gateway_deployment" "api_deployment" {
   rest_api_id = aws_api_gateway_rest_api.my_api.id
-  stage_name  = "dev"
+  aws_api_gateway_stage = "dev"
 
   triggers = {
     redeployment = sha1(jsonencode([
