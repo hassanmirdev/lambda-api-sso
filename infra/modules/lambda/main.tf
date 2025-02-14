566038403a6caf5f5f573ec1c7e8b54d797eebb9
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-14 10:00:49 +0530

    Update main.tf

--- infra/modules/lambda/main.tf
+++ infra/modules/lambda/main.tf
@@ -1,8 +1,9 @@
 #1 Create Lambda Function
+
 data "archive_file" "lambda_zip_file" {
   type        = "zip"
-  source_file = "lambda/index.js"
-  output_path = "lambda/index.zip"
+  source_file = "app/index.js"
+  output_path = "app/index.zip"
 }
 
 resource "aws_iam_role" "lambda_role" {
@@ -15,8 +16,8 @@ resource "aws_iam_role_policy_attachment" "lambda_exec_role_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-resource "aws_lambda_function" "yt_lambda_function" {
-  filename         = "lambda/index.zip"
+resource "aws_lambda_function" "my_lambda_function" {
+  filename         = app/index.zip"
   function_name    = "DemoLambdaFunction"
   role             = aws_iam_role.lambda_role.arn
   handler          = "index.handler"
@@ -26,14 +27,14 @@ resource "aws_lambda_function" "yt_lambda_function" {
 
   environment {
     variables = {
-      VIDEO_NAME = "Lambda Terraform Demo"
+     NAME = "Lambda-API-SSO-Demo"
     }
   }
 }
 
 #2 Create API Gateway
-resource "aws_api_gateway_rest_api" "yt_api" {
-  name        = "yt-api"
+resource "aws_api_gateway_rest_api" "my_api" {
+  name        = "my_api"
   description = "API for Demo"
 
   endpoint_configuration {
@@ -41,36 +42,36 @@ resource "aws_api_gateway_rest_api" "yt_api" {
   }
 }
 
-resource "aws_api_gateway_resource" "yt_api_resource" {
-  parent_id   = aws_api_gateway_rest_api.yt_api.root_resource_id
+resource "aws_api_gateway_resource" "my_api_resource" {
+  parent_id   = aws_api_gateway_rest_api.my_api.root_resource_id
   path_part   = "demo-path"
-  rest_api_id = aws_api_gateway_rest_api.yt_api.id
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
 }
 
-resource "aws_api_gateway_method" "yt_method" {
-  resource_id   = aws_api_gateway_resource.yt_api_resource.id
-  rest_api_id   = aws_api_gateway_rest_api.yt_api.id
+resource "aws_api_gateway_method" "my_method" {
+  resource_id   = aws_api_gateway_resource.my_api_resource.id
+  rest_api_id   = aws_api_gateway_rest_api.my_api.id
   http_method   = "ANY"  # Allow all HTTP methods
   authorization = "NONE" # Adjust if you need authorization
 }
 
 resource "aws_api_gateway_integration" "lambda_integration" {
-  http_method             = aws_api_gateway_method.yt_method.http_method
-  resource_id             = aws_api_gateway_resource.yt_api_resource.id
-  rest_api_id             = aws_api_gateway_rest_api.yt_api.id
+  http_method             = aws_api_gateway_method.my_method.http_method
+  resource_id             = aws_api_gateway_resource.my_api_resource.id
+  rest_api_id             = aws_api_gateway_rest_api.my_api.id
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.yt_lambda_function.invoke_arn
+  uri                     = aws_lambda_function.my_lambda_function.invoke_arn
 }
 
 resource "aws_api_gateway_deployment" "api_deployment" {
-  rest_api_id = aws_api_gateway_rest_api.yt_api.id
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
   stage_name  = "dev"
 
   triggers = {
     redeployment = sha1(jsonencode([
-      aws_api_gateway_resource.yt_api_resource.id,
-      aws_api_gateway_method.yt_method.id,
+      aws_api_gateway_resource.my_api_resource.id,
+      aws_api_gateway_method.my_method.id,
       aws_api_gateway_integration.lambda_integration.id
     ]))
   }
@@ -87,9 +88,9 @@ resource "aws_api_gateway_deployment" "api_deployment" {
 resource "aws_lambda_permission" "apigw_lambda_permission" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.yt_lambda_function.function_name
+  function_name = aws_lambda_function.my_lambda_function.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.yt_api.execution_arn}/*/*"  # Matches all methods on all paths
+  source_arn    = "${aws_api_gateway_rest_api.my_api.execution_arn}/*/*"  # Matches all methods on all paths
 }
 
 output "invoke_url" {
